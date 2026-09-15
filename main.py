d218b2fd53cf1066702f792e55e6502cc3ff8521
Gabriel Conde <gabriel.wave.conde@gmail.com>
2026-09-15 15:58:51 -0300

    fast undo

--- main.py
+++
@@ -1,7 +0,0 @@
-from requests import Request
-def main(request:Request):
-    print(request.headers.get("nomenome"))
-    print(request.get_json())
-    print(request.headers.items())
-    # print(request.)
-    return {"status": 200, "msg": "opa"}
