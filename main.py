0a783be1df246a90678ab55e23e05f1dd763a09c
Gabriel Conde <gabriel.wave.conde@gmail.com>
2026-09-15 15:34:00 -0300

    teste ver request

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ def main(request):
     print(request.headers.get("nomenome"))
     print(request.get_json())
     # print(request.)
-    return {"status": 200, "msg": "opa"}
+    return {"status": 200, "msg": "opa", "dados": request.get_json()}
