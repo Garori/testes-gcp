c1d5bb8eb613b94387f671e617349160073e9d2c
Gabriel Conde <gabriel.wave.conde@gmail.com>
2026-09-15 16:07:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 def main(request):
     print(request.headers.get("nomenome"))
     print(request.get_json())
-    print(request.headers.items())
+    for item in request.header.items():
+        print(item)
     # print(request.)
     return {"status": 200, "msg": "opa"}
