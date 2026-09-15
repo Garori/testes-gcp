d635232e26a0d6300aafa53c46249c615e6b3980
Gabriel Conde <gabriel.wave.conde@gmail.com>
2026-09-15 16:51:03 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ def main(request):
             # header and caches preflight response for an 3600s
             headers = {
                 "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "GET",
+                "Access-Control-Allow-Methods": "POST",
                 "Access-Control-Allow-Headers": "Content-Type",
                 "Access-Control-Max-Age": "3600",
             }
