034d0d879d06284cff313269ffae73f1af3764a2
Gabriel Conde <gabriel.wave.conde@gmail.com>
2026-09-15 17:00:14 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,7 @@
 def main(request):
-    if request.method == "OPTIONS":
-            # Allows GET requests from any origin with the Content-Type
-            # header and caches preflight response for an 3600s
-            headers = {
-                "Access-Control-Allow-Origin": "*",
-                "Access-Control-Allow-Methods": "POST",
-                "Access-Control-Allow-Headers": "Content-Type",
-                "Access-Control-Max-Age": "3600",
-            }
-
-            return ("", 204, headers)
-
-    # Set CORS headers for the main request
-    headers = {"Access-Control-Allow-Origin": "*"}
-
     print(request.headers)
     print(request.get_json())
     for item in request.headers.items():
         print(item)
     # print(request.)
-    return ("Hello World!", 200, headers)
+    return {"status": 200, "msg": "opa"}
